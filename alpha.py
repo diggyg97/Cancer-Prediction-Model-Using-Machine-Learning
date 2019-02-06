bef5d618b86eaa62c40f13eab46efbe04f1c2dc4
Digvijay Ghotane <digvijay.ghotane@gmail.com>
2019-02-06 15:01:50 +0530

    Third

--- alpha.py
+++ alpha.py
@@ -22,8 +22,22 @@ def csv_display(directory,index,z):
 
 a = int_check(1,4,"\nAre you:\n\n1. A Person looking for likelihood of Cancer?\n2. An Insurance Company?\n3. A Medical Researcher?\n4. A Pharmaceutical Company?\n\nPlease choose the integer corresponding to the dataset you'd like to access: ")
 if a==1:
-    s = pd.read_csv('/Users/digvijayghotane/Downloads/USCS_1999_2015_ASCII/BYAGE.txt',delimiter='|')
-    print(s)
+    gender = int_check(1,3,"\nThis prediction model will predict the probability of the likelihood of you being affected by lung cancer. We will ask from you, a series of inputs, please input the corresponding values as per your need.\n\nFirst Parameter:\nWhat is your gender?\n1. Female\n2. Male\n3. Other\n\nPlease Enter an integer corresponding to your choice: ")
+    age = int_check(1,10,"\nSecond Parameter:\nWhat age group do you belong to?:\n1. 18-40\n2. 40-45\n3. 45-50\n4. 50-55\n5. 55-60\n6. 60-65\n7. 65-70\n8. 70-75\n9. 75-80\n10. 80+\nPlease enter the integer corresponding to your age group: ")
+    smoking = int_check(1,2, "\nThird Paramenter:\nDo you smoke?\n1. No\n2. Yes\nPlease enter the integer corresponding to your smoking preference: ")
+    fingers = int_check(1,2,"\nFourth Parameter:\nDo you have yellow Fingers?\n1. No\n2. Yes\nPlease enter the corresponding integer: ")
+    anxiety = int_check(1,2,"\nFifth Parameter:\nHave you been medically diagnosed with anxiety?\n1. No\n2. Yes\nPlease enter the corresponding integer: ")
+    peer_pressure = int_check(1,2,"\nSeventh Parameter:\nAre you regularly peer pressured for smoking?\n1. No\n2. Yes\nPlease enter the corresponding integer: ")
+    chronic = int_check(1,2,"\nEighth Parameter:\nDo you suffer from any chronic illness?\n1. No\n2. Yes\nPlease enter the corresponding integer: ")
+    fatigue = int_check(1,2,"\nNinth Parameter:\nDo you suffer from fatigue all the time?\n1. No\n2. Yes\nPlease enter the corresponding integer: ")
+    allergy = int_check(1,2,"\nTenth Parameter:\nDo you suffer from any allergies?\n1. No\n2. Yes\nPlease enter the corresponding integer: ")
+    wheeze = int_check(1,2,"\nEleventh Parameter:\nDo you wheeze?\n1. No\n2. Yes\nPlease enter the corresponding integer: ")
+    alcohol = int_check(1,2,"\nTwelfth Parameter:\nDo you consume alcohol?\n1. No\n2. Yes\nPlease enter the corresponding integer: ")
+    coughing = int_check(1,2,"\nThirteenth Parameter:\nDo you suffer from cough?\n1. No\n2. Yes\nPlease enter the corresponding integer: ")
+    breath = int_check(1,2,"\nFourteenth Parameter:\nDo you suffer from shortness of breath?\n1. No\n2. Yes\nPlease enter the corresponding integer: ")
+    swallow = int_check(1,2,"\nFifteenth Parameter:\nDo you have difficulty in swallowing?\n1. No\n2. Yes\nPlease enter the corresponding integer: ")
+    chest = int_check(1,2,"\nSixteenth Parameter:\nDo you suffer from chest pain?\n1. No\n2. Yes\nPlease enter the corresponding integer: ")
+    s = pd.read_csv('/Users/digvijayghotane/Desktop/codecell/lung-cancer-data.csv')
 elif a==2:
     print("u noob")
 elif a==3:
